de9ed05440dca8f573f916a7b59866ab089927b5
gurganious <gurganious@yahoo.com>
2020-09-20 07:06:53 -0400

    Update main.py
    Improved algorithm using DataFrame outer merge/join

--- src/main.py
+++ src/main.py
@@ -1,33 +1,27 @@
-# 9/17/2020
-# Add default mapping
+from itertools import groupby
+from pandas import DataFrame, merge
+import re
 
-import pandas as pd
-
-def _flatten_dict(d):
+#################################################################################
+# Helper functions
+#################################################################################
+def _natural_sort_key(s, pattern = re.compile(r'[^\d]+|\d+')):
     '''
-        Flattens dictionary to leaf nodes only
+        Sort key for strings based upon number of digits and
+        ordering.
+        Splits with numbers and non-numbers into list sublists
+        of numbers only and non-numbers only
+        (i.e. s = "Jobs_$0_Task_$0_Logs
+              returns [5, 'Jobs_$', 0, '_Task_$', 0, '_Logs']")
     '''
-    def flatten(x, prefix = ''):
-        ' helper function to recursively flatten dictionary to leaf nodes only '
-        if isinstance(x, dict):
-            for a in x:
-                flatten(x[a], prefix + a + '_')
-        elif isinstance(x, list):
-            for i, k in enumerate(x):
-                flatten(k, prefix + '$' + str(i) + '_')
-        else:
-            out[prefix[:-1]] = x
-
-    out = {}
-    flatten(d)
-    return out
-
+    t = [int(x) if x.isnumeric() else x for x in pattern.findall(s)] 
+    return [_get_base(s), ] + t
 
 def _get_base(k):
     '''
         Find key with numeric separators removed
     '''
-    if "_" in k:
+    if "$" in k:
         s = '_'.join(k.split('_')[::2])  # even indexes (i.e. 'Task_$0_Name -> 'Task_Name')
     else:
         s = k
@@ -54,12 +48,11 @@ def _update_maps(k, forward_, reverse_):
         (use set since not unique without numeric separators)
     '''
     arr = k.split('_')
-    ok = False
     for i in range(1, len(arr)+1):
         # Going backwards thorugh k, find unique key which has not
         # been used yet in out dictionary
         suffix = '_'.join(arr[-i:])
-        if not suffix in forward_:
+        if not suffix in reverse_:
             break
             
     while suffix in reverse_:
@@ -70,100 +63,104 @@ def _update_maps(k, forward_, reverse_):
                    
     return forward_, reverse_
 
-def _shortest_keys(keys, default_map = None):
+#################################################################################
+# Core functions
+#################################################################################
+def _flatten_dict(d, no_expansion_columns):
     '''
-        :param keys        - keys from flattened dictionary
-        :param default_map - default mapping of base key name to new names
-        
-        Maps keys to shorter form so they can be used for column names
-        Based upon smallest key suffix that does not overlap with other keys
-        suffix can be only separated at '_' boundaries in string (i.e. so on whole words)
+        Flattens dictionary to leaf nodes only
     '''
-    if default_map is None:
-        default_map = {}
-        
-    # Convert flattened key names to basic names by removing 
-    # numeric sepators such as
-    # 'Task_$1_Logs_$0_name' -> 'Task_Logs_name'
-    base_keys = _get_base_keys(keys)
-  
-    # Init forward and reverse lookup
-    forward_ = {k:v for k, v in default_map.items()}
-    reverse_ = {v:k for k, v in forward_.items()}
-    
-    # Compute keys
-    for k in base_keys:
-        if k not in default_map:
-            _update_maps(k, forward_, reverse_)
-            
-    return forward_, reverse_
+    def flatten(x, prefix = ''):
+        ' helper function to recursively flatten dictionary to leaf nodes only '
+        if isinstance(x, dict):
+            for a in x:
+                if a in no_expansion_columns:
+                    out.setdefault(_get_base(prefix + a), []).append(x[a])
+                else:
+                    flatten(x[a], prefix + a + '_')
+        elif isinstance(x, list):
+            for i, k in enumerate(x):
+                flatten(k, prefix + '$' + str(i) + '_')
+        else:
+            # Place common leaf values in list
+            out.setdefault(_get_base(prefix[:-1]), []).append(x)
 
-def _merge_keys(fd, default_map = None):
-    '''
-        :param fd     - flattened dictionary
-        :default_map - default map of base keys to new key words
-        :return      - dictionary with items aggregated by key
-        
-        Merges compatible keys in flattened dictionary
-        Example:
-           {
-             'Task_$0_Name': 1,
-             'Task_$1_Name' : 2
-           }
-        becomes:
-            {
-             'Name': [1, 2]
-           }
-        where 'Name' is the shortest unique suffix name (ignore _$d_)
-    '''
-    
-    # Find shortest key set for flattened keys (i.e. shortest names for compatible key set)
-    forward_, reverse_ = _shortest_keys(fd.keys(), default_map)
-    
-    # Aggregate items together based upon having the same key
     out = {}
-    for k, value in fd.items():
-        # Base key (skipping numbers i.e. )
-        base = _get_base(k)  # i.e. 'Task_$1_Logs_$2_name' -> 'Task_Logs_name'
-        
-        suffix = forward_[base]
-        
-        # Add to column values for shortened key
-        out.setdefault(suffix, []).append(value)
-            
+    flatten(d)
+    
+    # Sort keys in natural order
+    out = dict(sorted(out.items(), key=lambda kv: _natural_sort_key(kv[0].lower())))
+    
     return out
 
-def _repeat(d):
+def _form_dictionaries(fd):
     '''
-        Repeats elements in dictionary list values to all have the same length (assume fixed multiple)
-        Inputs:
-            d - dictionary containing keys and values (each a list)
-        Output:
-            dictionary with values updated
+        Forms dictionaries of similar keys from flattened dictionary
+    
     '''
-    # determine length of largest list
-    max_ = max(len(v) for k, v in d.items())
+    # Find forward lookup table for unique names that base_keys (i.e. path to leaf name) can be shorted too
+    # Maps base name to shorter name i.e. Jobs_job - > job
+    base_keys = _get_base_keys(fd.keys())
+    forward_ = {}
+    reverse_ = {}
+    for k in base_keys:
+        _update_maps(k, forward_, reverse_)
+        
+    # Group keys by number of separators (i.e. length of path to leaf)
+    sorted_fd = sorted(fd.items(), key = lambda kv: len(kv[1]))
+    g = groupby(sorted_fd, lambda kv: len(kv[1]))
+    # groups of (2, [('Jobs_job', [1, 2]), ('Jobs_jobname', ['jobname', 'jobname']), etc.
+
+    out = {}
+    for cnt, subgroup in g:
+        # Outer dictionary is just the length of path for group i.e. 'Jobs_job' has path length 2
+        out[cnt] = {'_$mergekey': [1]*cnt}   # inject a merge key for use by DataFrame join in _form_dataframes
+        for v in subgroup:
+            v = list(v)
+            out[cnt][forward_[v[0]]] = v[1]
     
-    # Repeat each item in list (i.e. [item for item in v for _ in range(max_//len(v))] )
-    return {k:[item for item in v for _ in range(max_//len(v))] for k, v in d.items()}
+    return out
+
+def _form_dataframes(groups_by_count):
+    '''
+        Forms Dataframes of items with similar number of values in item (by count)
     
-def nested_dict_to_df(d, default_map = None):
+    '''
+    def _join(df1, df2):
+        return merge(df1, df2, on = '_$mergekey')
+    
+    def _reduce(function, iterable, initializer=None):
+        ' code equivalent of functools reduces'
+        # In case you don't have functools.reduce
+        it = iter(iterable)
+        if initializer is None:
+            value = next(it)
+        else:
+            value = initializer
+        for element in it:
+            value = function(value, element)
+        return value
+
+    # Convert each dictionary to DataFrame which have the same lenght
+    dfs = (DataFrame(dic) for cnt, dic in  groups_by_count.items())
+   
+    # Perform outer join on DataFrame (i.e. merging different lengths)
+    df = _reduce(_join, dfs)
+    
+    # Drop merge key column
+    df = df.drop('_$mergekey', axis = 1)
+
+    return df
+    
+#################################################################################
+# Main function
+#################################################################################
+def nested_dict_to_df(d, *no_expansion_columns):
     '''
         Converts a nested dictionary to dataframe
     '''
     # Convert nested dictionary to table form
-    table = _repeat(_merge_keys(_flatten_dict(d), default_map))
-    
-    return pd.DataFrame(table)
+    fd = _flatten_dict(d, no_expansion_columns)            # Flattened dictionaries
+    dics = _form_dictionaries(fd)                          # Merge based upon path length to leaf nodes
+    return _form_dataframes(dics)                          # Merge dictionaries using DataFrame outer join
    
-def show_default_name_map(d):
-    '''
-        Shows the default mapping of names for dictionary
-    '''
-    fd = _flatten_dict(d)
-    
-    forward, _ = _shortest_keys(fd)
-    
-    for k, v in forward.items():
-        print(f'{k} : {v}')
-        

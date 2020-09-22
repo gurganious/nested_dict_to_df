5c53e0dd4c184ae30c02ea58f63e436ebf3c2f37
gurganious <gurganious@yahoo.com>
2020-09-22 13:31:50 -0400

    Update main.py
    Revised algorithm

--- src/main.py
+++ src/main.py
@@ -1,11 +1,31 @@
+# 9/22/2020
 from itertools import groupby
+import pandas as pd
 from pandas import DataFrame, merge
 import re
 
 #################################################################################
+# Constants
+#################################################################################
+Merge_Key = '_$mergekey'
+
+#################################################################################
 # Helper functions
 #################################################################################
-def _natural_sort_key(s, pattern = re.compile(r'[^\d]+|\d+')):
+def _get_iters(s, pattern = re.compile(r'[^\d]+|\d+')):
+    '''
+        List of numerical list iterations in field name
+        i.e. a_$1_x_$2 -> [1, 2]
+    '''
+    return [int(x) if x.isnumeric() else x for x in pattern.findall(s)] 
+    
+def _first_iter(k):
+    '''
+        First number in key token i.e. a_$1_b_$0 -> 1
+    '''
+    return _get_iters(k)[1]
+
+def _natural_sort_key(s):
     '''
         Sort key for strings based upon number of digits and
         ordering.
@@ -14,8 +34,7 @@ def _natural_sort_key(s, pattern = re.compile(r'[^\d]+|\d+')):
         (i.e. s = "Jobs_$0_Task_$0_Logs
               returns [5, 'Jobs_$', 0, '_Task_$', 0, '_Logs']")
     '''
-    t = [int(x) if x.isnumeric() else x for x in pattern.findall(s)] 
-    return [_get_base(s), ] + t
+    return [_depth(s), _get_base(s), ] + _get_iters(s)
 
 def _get_base(k):
     '''
@@ -63,9 +82,60 @@ def _update_maps(k, forward_, reverse_):
                    
     return forward_, reverse_
 
+def _minimize_names(names):
+    '''
+        Finds set of minimum width names by removing prefix as long as names don't collide
+    '''
+    # Init forward and reverse lookup
+    forward_ = {}
+    reverse_ = {}
+    
+    # Compute keys
+    for k in names:
+        _update_maps(k, forward_, reverse_)
+            
+    return forward_
+
 def _depth(k):
+    '''
+        Depth measure by number of underscores in base name
+    '''
     return len(_get_base(k).split('_'))
 
+def _first_iter(k):
+    '''
+        First number in key token i.e. a_$1_b_$0 -> 1
+    '''
+    return _get_iters(k)[1]
+    
+
+def _sort_key(kv):
+    '''
+    Key function used to sort flattened dictionary
+    '''
+    t = _get_iters(kv[0])
+    if len(t) <= 1:
+        return (_depth(kv[0]),)
+    else:
+        t = t[1::2]  # list indexes only
+        #t = t[:-1]   # leave out last element
+        return  (_depth(kv[0]),) + tuple(t)
+    
+def _group_key(kv):
+    '''
+    Key function used to group flattened dictionary
+    '''
+    if isinstance(kv, list):
+        kv = kv[0]
+    t = _get_iters(kv[0])
+    if len(t) <= 1:
+        return (_depth(kv),)
+    else:
+        t = t[1::2]  # list indexes only
+        t = t[:-1]   # leave out last element
+        return  (_depth(kv[0]),) + tuple(t)
+    
+    
 #################################################################################
 # Core functions
 #################################################################################
@@ -78,7 +148,8 @@ def _flatten_dict(d, *no_expansion_columns):
         if isinstance(x, dict):
             for a in x:
                 if a in no_expansion_columns:
-                    out.setdefault(_get_base(prefix + a), []).append(x[a])
+                    #out.setdefault(_get_base(prefix + a), []).append(x[a])
+                    out[prefix + a] = str(x[a])
                 else:
                     flatten(x[a], prefix + a + '_')
         elif isinstance(x, list):
@@ -86,7 +157,8 @@ def _flatten_dict(d, *no_expansion_columns):
                 flatten(k, prefix + '$' + str(i) + '_')
         else:
             # Place common leaf values in list
-            out.setdefault(_get_base(prefix[:-1]), []).append(x)
+            #out.setdefault(prefix[:-1], []).append(x)
+            out[prefix[:-1]] = x
 
     out = {}
     flatten(d)
@@ -110,52 +182,91 @@ def _form_dictionaries(fd):
         _update_maps(k, forward_, reverse_)
         
     # Group keys by number of separators (i.e. length of path to leaf)
-    sorted_fd = sorted(fd.items(), key = lambda kv: (_depth(kv[0]), len(kv[1])))
-    g = groupby(sorted_fd, lambda kv: (_depth(kv[0]), len(kv[1])))
-    # groups of (2, [('Jobs_job', [1, 2]), ('Jobs_jobname', ['jobname', 'jobname']), etc.
+    sorted_fd = sorted(fd.items(), key = _sort_key)
+    g = groupby(sorted_fd, _group_key)
+    # (2, 1) [('Jobs_$0_job', [1]), ('Jobs_$1_job', [2]), ('Jobs_$0_jobname', ['jobname']), ('Jobs_$1_jobname', ['jobname']), 
 
     out = {}
     for kval, subgroup in g:
         # Outer dictionary is just the length of path for group i.e. 'Jobs_job' has path length 2
-        out[kval] = {'_$mergekey': [1]*kval[1]}   # inject a merge key for use by DataFrame join in _form_dataframes
-                                                  # based upon number of items in list
+        out[kval] = {}   
+            
+        subgroup = list(subgroup)
         for v in subgroup:
             v = list(v)
-            out[kval][forward_[v[0]]] = v[1]
-    
+            out[kval].setdefault(_get_base(v[0]), []).append(v[1])
+        
+        # Add merge key based upon first item in group
+        first = set()
+        for v in subgroup:
+            v = list(v)
+            base = _get_base(v[0])
+            if not first or base in first:
+                out[kval].setdefault(f'{Merge_Key}', []).append(_first_iter(v[0]))
+                first.add(base)
+           
     return out
 
-def _form_dataframes(groups_by_count):
+def _form_dataframes(dics):
     '''
         Forms Dataframes of items with similar number of values in item (by count)
     
     '''
     def _join(df1, df2):
-        return merge(df1, df2, on = '_$mergekey')
+        '''
+            Outer merge join of two dataframes
+        '''
+        if df1 is None:
+            return df2
+        if df2 is None:
+            return df1
+        return merge(df1, df2, on = Merge_Key)
     
-    def _reduce(function, iterable, initializer=None):
-        ' code equivalent of functools reduces'
-        # In case you don't have functools.reduce
-        it = iter(iterable)
-        if initializer is None:
-            value = next(it)
+    def _stack(df1, df2):
+        '''
+            vertically stack two data frames
+        '''
+        if df1 is None:
+            return df2
+        if df2 is None:
+            return df1
+        return pd.concat([df1, df2], ignore_index=True)
+
+
+    # Group dics by depth
+    k_lst, df_result, df_current, df_accum = None, None, None, None
+    
+    for i, (k, d) in enumerate(dics.items()):
+        # Current dataframe from dictionary
+        df_current = DataFrame(d)
+        
+        if not k_lst:
+            k_lst = k[0]     # index of current group
+            df_accum = df_current
+        elif  k[0] == k_lst:
+            # Vertical stack DataFrames in same group
+            df_accum =_stack(df_accum, df_current)
+            
         else:
-            value = initializer
-        for element in it:
-            value = function(value, element)
-        return value
-
-    # Convert each dictionary to DataFrame which have the same lenght
-    dfs = (DataFrame(dic) for kval, dic in  groups_by_count.items())
+            # Merge current group with previous merge groups
+            df_result = _join(df_result, df_accum)
+            
+            # Reset accumulate of items in same group
+            df_accum = df_current
+            k_lst = k[0]
    
-    # Perform outer join on DataFrame (i.e. merging different lengths)
-    df = _reduce(_join, dfs)
-    
+        if i == len(dics.items()) - 1:
+            df_result = _join(df_result, df_accum)
+            
     # Drop merge key column
-    df = df.drop('_$mergekey', axis = 1)
-
-    return df
+    df_result = df_result.drop(f'{Merge_Key}', 1)
     
+    # Minimize names
+    rename_map = _minimize_names(df_result.columns)
+    df_result.rename(columns = rename_map, inplace = True)
+    
+    return df_result
+            
 #################################################################################
 # Main function
 #################################################################################
@@ -165,6 +276,6 @@ def nested_dict_to_df(d, *no_expansion_columns):
     '''
     # Convert nested dictionary to table form
     fd = _flatten_dict(d, *no_expansion_columns)            # Flattened dictionaries
-    dics = _form_dictionaries(fd)                          # Merge based upon path length to leaf nodes
-    return _form_dataframes(dics)                          # Merge dictionaries using DataFrame outer join
-   
+    dics = _form_dictionaries(fd)                          # Aggregate nodes along same path based upon names
+    return _form_dataframes(dics)                          # Merge dictionaries using DataFrame outer join and concat
+ 
